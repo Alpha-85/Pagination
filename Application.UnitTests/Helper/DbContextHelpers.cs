4856dbb3f0f280d52337f6dc2dc32efb729a169d
Alpha-85 <ljungdahlt@gmail.com>
2022-03-21 20:10:13 +0100

    Added UnitTests For Handler

---
+++ Application.UnitTests/Helper/DbContextHelpers.cs
@@ -0,0 +1,16 @@
+﻿using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UnitTests.Helper;
+
+public static class DbContextHelpers
+{
+    public static ApplicationDbContext GetApplicationDbContext()
+    {
+        var optionsBuilder = new DbContextOptionsBuilder();
+        optionsBuilder.UseInMemoryDatabase(Guid.NewGuid().ToString());
+        var applicationDbContext = new ApplicationDbContext(optionsBuilder.Options);
+
+        return applicationDbContext;
+    }
+}
---
+++ Application.UnitTests/Helper/MemberObjectBuilder.cs
@@ -0,0 +1,17 @@
+﻿using AutoFixture;
+using Domain.Models;
+
+namespace Application.UnitTests.Helper;
+
+public static class MemberObjectBuilder
+{
+    public static IEnumerable<Member> AddMembers(this IFixture fixture)
+    {
+        var members = fixture
+            .Build<Member>()
+            .WithAutoProperties()
+            .CreateMany(100);
+
+        return members;
+    }
+}
